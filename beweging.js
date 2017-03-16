2a61359825192df2597dc1e1fc6ae2356b3b235e
Jannevhooff <jannevanhooff@hotmail.com>
2017-03-16 12:53:49 +0100

    upload of project

---
+++ beweging.js
@@ -0,0 +1,47 @@
+ $( function() {
+        $( "#balk" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk1" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk2" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk3" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk4" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk5" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk6" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk7" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk8" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk9" ).draggable();
+    } );
+
+ $( function() {
+        $( "#balk10" ).draggable();
+    } );
+
+$('.randomElement').each(function(){
+    $(this).css({'marginTop' : getRandomInt(-100,600), 'marginTop' : getRandomInt(-100,600)});
+});
